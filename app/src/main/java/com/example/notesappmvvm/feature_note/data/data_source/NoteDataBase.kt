b90905c8eccfa01f7130b84b3f0c88dc24f38280
bharathraj <bharathraj16903@gmail.com>
2025-01-27 20:40:08 +0530

    Create NoteDataBase

---
+++ app/src/main/java/com/example/notesappmvvm/feature_note/data/data_source/NoteDataBase.kt
@@ -0,0 +1,15 @@
+package com.example.notesappmvvm.feature_note.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.notesappmvvm.feature_note.domain.model.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class NoteDataBase:RoomDatabase() {
+    abstract val noteDao:NoteDao
+
+}
