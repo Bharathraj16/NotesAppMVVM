33cee252f1f199a9264463e3282406f755fa39ac
bharathraj <bharathraj16903@gmail.com>
2025-01-28 09:20:18 +0530

    Add AppModule

---
+++ app/src/main/java/com/example/notesappmvvm/di/AppModule.kt
@@ -0,0 +1,42 @@
+package com.example.notesappmvvm.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.notesappmvvm.feature_note.data.data_source.NoteDataBase
+import com.example.notesappmvvm.feature_note.data.repository.NoteRepositoryImpl
+import com.example.notesappmvvm.feature_note.domain.repository.NoteRepository
+import com.example.notesappmvvm.feature_note.domain.use_case.DeleteNote
+import com.example.notesappmvvm.feature_note.domain.use_case.GetNotes
+import com.example.notesappmvvm.feature_note.domain.use_case.NoteUseCases
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app:Application):NoteDataBase{
+        return Room.databaseBuilder(
+            app,
+            NoteDataBase::class.java,
+            NoteDataBase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db:NoteDataBase):NoteRepository{
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+}
