047d11f7d445a9725f105748f103d92389ce4129
bharathraj <bharathraj16903@gmail.com>
2025-01-28 09:21:05 +0530

    Add NotesViewModel

---
+++ app/src/main/java/com/example/notesappmvvm/feature_note/presentation/notes/components/NotesViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.notesappmvvm.feature_note.presentation.notes.components
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NotesViewModel @Inject constructor(
+
+):ViewModel(){
+
+}
